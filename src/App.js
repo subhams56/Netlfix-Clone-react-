49be3a426c1c785cbe8756f2a74af76828db9779
Subham Saha <89207473+subhams56@users.noreply.github.com>
2022-12-14 00:18:04 +0530

    Add files via upload
    Rest Upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './requests'
+import './App.css';
+import Row from './Row';
+import requests from './requests';
+import  Banner  from './Banner';
+import Nav from './Nav';
+
+
+
+function App() {
+  return (
+    <div className="app">
+       <Nav/>
+      <Banner/>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+      <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumantaries}/>
+      
+      
+      
+    </div>
+  );
+}
+
+export default App;
